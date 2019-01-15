1aa3b54616794c4ecb0ff9d47c51894f75ace9a3
MacBook di Enrico <Enrico@MacBook-di-MacBook.local>
2019-01-15 09:42:35 +0900

    change default background color

--- script.js
+++ script.js
@@ -9,6 +9,6 @@ color1.addEventListener('input',setGradient);
 color2.addEventListener('input', setGradient);
 
 function setGradient() {
-	body.style.background = `linear-gradient(to right, ${color1.value}, ${color2.value})`;
+	body.style.background = `linear-gradient(to bottom, ${color1.value}, ${color2.value})`;
 	css.textContent = body.style.background + ';';
 }
