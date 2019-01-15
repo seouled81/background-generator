d4b635aec626cce8f4c7d134c97318a94d78ed5d
MacBook di Enrico <Enrico@MacBook-di-MacBook.local>
2019-01-16 07:58:38 +0900

    add es6 syntax and new default bgColor

--- script.js
+++ script.js
@@ -1,14 +1,25 @@
-var body = document.getElementById('gradient');
-var css = document.querySelector('h3');
-var color1 = document.querySelector('.color1');
-var color2 = document.querySelector('.color2');
+const body = document.getElementById('gradient');
+const css = document.querySelector('h3');
+const color1 = document.querySelector('.color1');
+const color2 = document.querySelector('.color2');
 
+const init = () => {
+	color1.value = '#2bc0e4';
+	color2.value = '#eaecc6';
+	body.style.background = `linear-gradient(to bottom, ${color1.value} 25%, ${color2.value} 75%)`;
+	css.textContent = body.style.background + ';';
+}
+
+const setGradient = () => {
+	body.style.background = `linear-gradient(to bottom, ${color1.value}, ${color2.value})`;
+	css.textContent = body.style.background + ';';
+}
+
+init();
 
 color1.addEventListener('input',setGradient);
 
 color2.addEventListener('input', setGradient);
 
-function setGradient() {
-	body.style.background = `linear-gradient(to bottom, ${color1.value}, ${color2.value})`;
-	css.textContent = body.style.background + ';';
-}
+
+
